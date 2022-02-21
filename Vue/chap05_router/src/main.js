8562823f3c4ca22aaa356c7a64a78e865543299c
CODE0000 <thfxm0601@gmail.com>
2022-02-21 17:13:36 +0900

    0221_2
    라우터

--- Vue/chap05_router/src/main.js
+++ Vue/chap05_router/src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-Vue.config.productionTip = false
+// npm i vue-router@3
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
